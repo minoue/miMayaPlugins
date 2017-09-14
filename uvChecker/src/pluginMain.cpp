9345ceb23c77bc46e9c1f7253b9a37c9cbd46bf5
minoue <michitaka.inoue@icloud.com>
2017-09-14 21:31:35 +0100

    Register syntax

--- uvChecker/src/pluginMain.cpp
+++ uvChecker/src/pluginMain.cpp
@@ -1,11 +1,10 @@
-#include <maya/MFnPlugin.h>
 #include "uvChecker.h"
-
+#include <maya/MFnPlugin.h>
 
 MStatus initializePlugin(MObject mObj)
 {
     MFnPlugin fnPlugin(mObj, "Michitaka Inoue", "0.0.1", "Any");
-    fnPlugin.registerCommand("checkUV", UvChecker::creater);
+    fnPlugin.registerCommand("checkUV", UvChecker::creater, UvChecker::newSyntax);
     return MS::kSuccess;
 }
 
