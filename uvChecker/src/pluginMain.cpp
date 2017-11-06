1a9e19856236bd4875cba8f6a9db3da9f07ed957
minoue <michitaka.inoue@icloud.com>
2017-11-06 21:17:22 +0000

    Merge findUvOverlap command to uvChecker

--- uvChecker/src/pluginMain.cpp
+++ uvChecker/src/pluginMain.cpp
@@ -1,16 +1,24 @@
 #include "uvChecker.h"
+#include "findUvOverlaps.h"
 #include <maya/MFnPlugin.h>
 
 MStatus initializePlugin(MObject mObj)
 {
+    MFnPlugin ovPlugin(mObj, "Michitaka Inoue", "0.1.0", "Any");
+    ovPlugin.registerCommand("findUvOverlaps", FindUvOverlaps::creater, FindUvOverlaps::newSyntax);
+
     MFnPlugin fnPlugin(mObj, "Michitaka Inoue", "0.0.1", "Any");
     fnPlugin.registerCommand("checkUV", UvChecker::creater, UvChecker::newSyntax);
+
     return MS::kSuccess;
 }
 
 MStatus uninitializePlugin(MObject mObj)
 {
+    MFnPlugin ovPlugin(mObj);
+    ovPlugin.deregisterCommand("findUvOverlaps");
     MFnPlugin fnPlugin(mObj);
     fnPlugin.deregisterCommand("checkUV");
+
     return MS::kSuccess;
 }
