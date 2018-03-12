2910555d86d2fbed846e44584c09815f991c1a43
minoue <michitaka.inoue@icloud.com>
2018-03-12 08:44:22 +0000

    Fix plugin names

--- uvChecker/src/pluginMain.cpp
+++ uvChecker/src/pluginMain.cpp
@@ -1,15 +1,15 @@
+#include "findUvOverlaps_old.h"
 #include "findUvOverlaps.h"
-#include "findUvOverlaps2.h"
 #include "uvChecker.h"
 #include <maya/MFnPlugin.h>
 
 MStatus initializePlugin(MObject mObj)
 {
     MFnPlugin ovPlugin(mObj, "Michitaka Inoue", "0.1.0", "Any");
-    ovPlugin.registerCommand("findUvOverlaps", FindUvOverlaps::creator, FindUvOverlaps::newSyntax);
+    ovPlugin.registerCommand("findUvOverlaps_old", FindUvOverlaps::creator, FindUvOverlaps::newSyntax);
 
     MFnPlugin ov2Plugin(mObj, "Michitaka Inoue", "0.1.0", "Any");
-    ovPlugin.registerCommand("findUvOverlaps2", FindUvOverlaps2::creator, FindUvOverlaps2::newSyntax);
+    ovPlugin.registerCommand("findUvOverlaps", FindUvOverlaps2::creator, FindUvOverlaps2::newSyntax);
 
     MFnPlugin fnPlugin(mObj, "Michitaka Inoue", "0.0.1", "Any");
     fnPlugin.registerCommand("checkUV", UvChecker::creator, UvChecker::newSyntax);
@@ -20,9 +20,9 @@ MStatus initializePlugin(MObject mObj)
 MStatus uninitializePlugin(MObject mObj)
 {
     MFnPlugin ovPlugin(mObj);
-    ovPlugin.deregisterCommand("findUvOverlaps");
+    ovPlugin.deregisterCommand("findUvOverlaps_old");
     MFnPlugin ov2Plugin(mObj);
-    ovPlugin.deregisterCommand("findUvOverlaps2");
+    ovPlugin.deregisterCommand("findUvOverlaps");
     MFnPlugin fnPlugin(mObj);
     fnPlugin.deregisterCommand("checkUV");
 
