ece4dfa9472f315e30545ff2c565d56c56095661
Michi Inoue <mtkinoue@gmail.com>
2022-07-12 09:41:57 +0000

    Add mirror blendshape button

--- symmetryTable/utils.py
+++ symmetryTable/utils.py
@@ -1,6 +1,7 @@
 import pickle
 from maya import cmds
 from pymel import core as pm
+from functools import partial
 
 
 PLUGIN_NAME = "symmetryTable.so"
@@ -67,6 +68,47 @@ def getBlendShapeNodes(*args):
     return blendShapes
 
 
+def mirrorBlendShapeWeights(*args):
+    TARGET = "blendShape1.inputTarget[0].baseWeights"
+
+    textField = args[0]
+    symDataPath = textField.getText()
+
+    numVerts = cmds.polyEvaluate(v=True)
+    plug = OpenMaya.MPlug()
+    sel = OpenMaya.MSelectionList()
+    sel.add(TARGET)
+    sel.getPlug(0, plug)
+
+    weights = []
+
+    p = OpenMaya.MPlug()
+    for i in range(numVerts):
+        p = plug.elementByLogicalIndex(i)
+        value = p.asFloat()
+        weights.append(value)
+
+    try:
+        with open(symDataPath, 'rb') as handle:
+            unserialized_data = pickle.load(handle)
+
+    except IOError:
+        print("No such file")
+        return
+
+    for i in unserialized_data:
+        leftIndex = i
+        leftWeight = weights[leftIndex]
+
+        rightIndex = unserialized_data[leftIndex]
+        weights[rightIndex] = leftWeight
+
+    for n, i in enumerate(weights):
+        p = OpenMaya.MPlug()
+        p = plug.elementByLogicalIndex(n)
+        p.setFloat(i)
+
+
 def gui():
     windowName = "symTools"
     if pm.window(windowName, q=True, exists=True):
@@ -76,7 +118,13 @@ def gui():
     pm.columnLayout(adj=True)
     pm.button("Create Sym Data", c=createSymData)
     pm.text("Blend shapes")
-    pm.button("Mirror blendshape weights")
+    symDataPath = pm.textField()
+    pm.button(
+        "Mirror blendshape weights",
+        command=partial(
+            mirrorBlendShapeWeights,
+            symDataPath
+        ))
     win.show()
 
 
